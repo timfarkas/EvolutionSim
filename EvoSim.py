dfcfce408a5b9acfe164baefe5f0770b8848b725
Johanna.Buth <Johannabuth@uni-konstanz.de>
2023-08-18 17:58:34 +0200

    Added Bottleneck

--- EvoSim.py
+++ EvoSim.py
@@ -1,24 +1,57 @@
 import numpy
 import matplotlib.pyplot as plt
 
+<<<<<<< HEAD
+    # variables for bottleneck
+bottleneckTimestamp = 200
+decimationFactor = 0.1
+
+=======
+>>>>>>> 303203e63ed05a0e0b6a53b4cd8a6b6f44f19c37
 def reproduce(i, n, s):
     p = i/n
     expectedP = (p*(1+s))/(p*(1+s)+(1-p))
     j = numpy.random.binomial(n, expectedP)
     return j
     
+# reproduction of growing population
+def reproduceGrowing(i, n, s, nOld):
+    if n < nOld:                            # growing population
+        p = i/n
+        j = numpy.random.binomial(n, p)
+        return j
+    else:
+        p = i/nOld                          # stagnating population
+        expectedP = (p*(1+s))/(p*(1+s)+(1-p))
+        j = numpy.random.binomial(nOld, expectedP)
+        return j
+
 ### takes initial mutants j, population size n, mutationbenefit s and no of generations
-def simulate(initialJ, n, mutationBenefit, generations = 1000):
+def simulate(initialJ, n, mutationBenefit, generations = 1000, bottleneckTimestamp = 500, decimationFactor = 1): # bottleneck variables added
+    # saves initial population size for bottleneck
+    nOld = n
     ## array simulation [currentGen, p, Bool fixation ]
     simulation = [[],[],None]
     j = initialJ
     for currentGen in range(0, generations):
-        newJ = reproduce(j, n, mutationBenefit)
-        #print("Generation: "+str(currentGen)+" "+ str(j))
-        j = newJ
+        if currentGen < bottleneckTimestamp-1:                                 # situation before bottleneck
+            newJ = reproduce(j, n, mutationBenefit)
+            j = newJ  
+        elif currentGen == bottleneckTimestamp-1:
+            print (decimationFactor)
+            n = int(n* decimationFactor)                            # decimates population size
+            j = numpy.random.binomial(n, p)                                 # calculates new j
+            p = j/n
+            newJ = reproduce(j, n, mutationBenefit)
+            j = newJ
+        else:                                                       # regrowing population size
+            newJ = reproduce(j, n, mutationBenefit)
+            #print("Generation: "+str(currentGen)+" "+ str(j))
+            j = newJ
+            
         p = j/n
         simulation[0].append(currentGen)
-        simulation[1].append(p)
+        simulation[1].append(p*n)
         if p == 0:
             simulation[2] = 0
             break
@@ -32,7 +65,7 @@ def simulate(initialJ, n, mutationBenefit, generations = 1000):
 def runSimulations(initialJ, n, mutationBenefit, generations = 1000, runs = 100):
     simulations = [[],0]
     for run in range(0, runs):
-        simulation = simulate(initialJ, n, mutationBenefit, generations)
+        simulation = simulate(initialJ, n, mutationBenefit, generations, bottleneckTimestamp, decimationFactor)     # bottleneck variables added
         simulations[0].append(simulation)
         simulations[1] = simulations[1] + simulation[2] 
     fixationProb = simulations[1]/simulations[0].__len__()
