c23c54476e1f4a5e1c9fed22b7cd3feee167051d
Johanna.Buth <Johannabuth@uni-konstanz.de>
2023-08-21 17:01:09 +0200

    Added growing Population

--- EvoSim.py
+++ EvoSim.py
@@ -13,16 +13,26 @@ def reproduce(i, n, s):
     return j
     
 # reproduction of growing population
-def reproduceGrowing(i, n, s, nOld):
-    if n < nOld:                            # growing population
-        p = i/n
-        j = numpy.random.binomial(n, p)
-        return j
+def reproduceGrowing(i, n, s, currentGen, bottleneckTimestamp):
+    c = 1000    # Capacity of the system
+    if currentGen < bottleneckTimestamp-1:                                   # calculation of new n
+        nNew = - c/(1 + numpy.exp(currentGen)) +n                               # before bottleneck
     else:
-        p = i/nOld                          # stagnating population
-        expectedP = (p*(1+s))/(p*(1+s)+(1-p))
-        j = numpy.random.binomial(nOld, expectedP)
-        return j
+        nNew = - c/(1 + numpy.exp(currentGen - bottleneckTimestamp)) +n         # after bottleneck
+    
+    p = i/n
+    expectedP = (p*(1+s))/(p*(1+s)+(1-p))
+    j = numpy.random.binomial(n, expectedP)
+    return j
+
+def reproduceGrowing2(i, n, s):
+    c = 1000    # Capacity of the system
+    nNew = int(n*(1-n/c))                       # before bottleneck
+    
+    p = i/n
+    expectedP = (p*(1+s))/(p*(1+s)+(1-p))
+    j = numpy.random.binomial(n, expectedP)
+    return j
 
 ### takes initial mutants j, population size n, mutationbenefit s and no of generations
 def simulate(initialJ, n, mutationBenefit, generations = 1000, bottleneckTimestamp = 500, decimationFactor = 1): # bottleneck variables added
@@ -33,17 +43,29 @@ def simulate(initialJ, n, mutationBenefit, generations = 1000, bottleneckTimesta
     j = initialJ
     for currentGen in range(0, generations):
         if currentGen < bottleneckTimestamp-1:                                 # situation before bottleneck
-            newJ = reproduce(j, n, mutationBenefit)
+            
+            #newJ = reproduce(j, n, mutationBenefit)                                             # constant population
+            #newJ = reproduceGrowing(j, n, mutationBenefit, currentGen, bottleneckTimestamp)     #growing population
+            newJ = reproduceGrowing2(j, n, mutationBenefit)                                   
+            
             j = newJ  
         elif currentGen == bottleneckTimestamp-1:
             print (decimationFactor)
             n = int(n* decimationFactor)                            # decimates population size
             j = numpy.random.binomial(n, p)                                 # calculates new j
             p = j/n
-            newJ = reproduce(j, n, mutationBenefit)
+            
+            #newJ = reproduce(j, n, mutationBenefit)                                             # constant population
+            #newJ = reproduceGrowing(j, n, mutationBenefit, currentGen, bottleneckTimestamp)     #growing population
+            newJ = reproduceGrowing2(j, n, mutationBenefit)
+            
             j = newJ
         else:                                                       # regrowing population size
-            newJ = reproduce(j, n, mutationBenefit)
+            
+            #newJ = reproduce(j, n, mutationBenefit)                                             # constant population
+            #newJ = reproduceGrowing(j, n, mutationBenefit, currentGen, bottleneckTimestamp)     #growing population
+            newJ = reproduceGrowing2(j, n, mutationBenefit)
+            
             #print("Generation: "+str(currentGen)+" "+ str(j))
             j = newJ
             
@@ -71,10 +93,11 @@ def runSimulations(initialJ, n, mutationBenefit, generations = 1000, runs = 100)
     return simulations
 
 
-simulations = runSimulations(50,1000,0.02, 1000, 100)
+simulations = runSimulations(initialJ = 50,n=1000, mutationBenefit = 0.02, generations = 1000, runs = 10)
 for simulation in simulations[0]:
     plt.plot(simulation[0],simulation[1])
 plt.xlabel("Generations")
+plt.axvline(x=bottleneckTimestamp,color = 'r',linestyle='--')
 plt.ylabel("p")
-plt.title("Fixation probability: "+ str(simulations[1]))
+plt.title("Fixation probability: "+ str(simulations[1]))zu
 plt.show()
