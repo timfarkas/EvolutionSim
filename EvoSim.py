f1bdc5db8bce2eed1c8fc8e6fb138ae4d1d95dc5
Johanna.Buth <Johannabuth@uni-konstanz.de>
2023-08-23 00:06:48 +0200

    Added sliders

--- EvoSim.py
+++ EvoSim.py
@@ -1,136 +1,126 @@
 import numpy
 import matplotlib.pyplot as plt
 
-def reproduce(i, n, s):
-    p = i/n
-    expectedP = (p*(1+s))/(p*(1+s)+(1-p))
-    j = numpy.random.binomial(n, expectedP)
-    return j
 
-#### takes population and proportion of mutants and outputs new proportion of mutants new population [j,n] based on growth factor r
-def reproduceGrowing(i, n, s, r=0.1):
-    c = 1000    # Capacity of the system
-    n = int((1+r)*n*(1-(r*n)/((1+r)*c)))                       # before bottleneck
+class EvoSim:
+    def __init__(self,parameters):
+        self.parameters = parameters
+        #self.combinedSimulation()
     
-    p = i/n
-    expectedP = (p*(1+s))/(p*(1+s)+(1-p))
-    j = numpy.random.binomial(n, expectedP)
-    return [j,n]
+    #### takes population and proportion of mutants and outputs new proportion of mutants new population [j,n] based on growth factor r
+    def reproduceGrowing(self, i, n, s, c ,r):
+        n = int((1+r)*n*(1-(r*n)/((1+r)*c)))                       # before bottleneck
+        p = i/n
+        expectedP = (p*(1+s))/(p*(1+s)+(1-p))
+        j = numpy.random.binomial(n, expectedP)
+        return [j,n]
 
-### takes initial mutants j, population size n, mutationbenefit s and no of generations
-def simulate(initialJ, n, mutationBenefit, generations = 1000, bottleneckTimestamp = 500, decimationFactor = 1, mode = None): #bottleneck variables added
-    # saves initial population size for bottleneck
-    nOld = n
-    ## array simulation [currentGen, p, Bool fixation ]
-    simulation = [[],[],0]
-    j = initialJ
-    for currentGen in range(0, generations):            
-        ### modify mutationBenefit based on environmentalBenefitModifier to simulate environmental effects on mutationBenefit
-        modS = mutationBenefit * environmentalBenefitModifier(currentGen,mutationBenefit,mode)
-        if currentGen < bottleneckTimestamp-1:      # situation before bottleneck
-            
-            #newJ = reproduce(j, n, modS)           # constant population  
-            array = reproduceGrowing(j, n, modS)     #growing population
-            j = array[0] 
-            n = array[1]  
-        elif currentGen == bottleneckTimestamp-1:
-            #print (decimationFactor)
-            nDecimated = int(n* decimationFactor)                            # decimates population size
-            j = numpy.random.binomial(nDecimated, p)                                 # calculates new j
-            p = j/nDecimated
-            #newJ = reproduce(j, nDecimated, modS)           # constant population  
-            array = reproduceGrowing(j, nDecimated, modS)     #growing population
-            j = array[0] 
-            n = array[1]
-        else:                                                       # regrowing population size
-            #newJ = reproduce(j, n, modS)           # constant population  
-            array = reproduceGrowing(j, n, modS)     #growing population
-            #print("Generation: "+str(currentGen)+" "+ str(j))
-            j = array[0] 
-            n = array[1]
-
+    ### takes initial mutants j, population size n, mutationbenefit s and no of generations
+    def simulate(self, initialJ, n, mutationBenefit, generations = 1000, bottleneckTimestamp = 500, decimationFactor = 1, mode = None,c = 1000,r = 0.1): #bottleneck variables added
+        ## array simulation [currentGen, p, Bool fixation ]
+        simulation = [[],[],0]
+        j = initialJ
         p = j/n
-        simulation[0].append(currentGen)
-        simulation[1].append(p*n)
-        if p == 0:
-            simulation[2] = 0
-            break
-        if p == 1:
-            #print("BREAK, n ="+str(n))
-            simulation[2] = 1
-            break
-    return simulation
+        for currentGen in range(0, generations):            
+            ### modify mutationBenefit based on environmentalBenefitModifier to simulate environmental effects on mutationBenefit
+            modS = mutationBenefit * self.environmentalBenefitModifier(currentGen,mutationBenefit,mode)
+            if currentGen == bottleneckTimestamp-1:
+                nDecimated = int(n* decimationFactor)                   # decimates population size
+                j = numpy.random.binomial(nDecimated, p)                # calculates new j
+                p = j/nDecimated  
+                array = self.reproduceGrowing(j, nDecimated, modS,c,r)       #growing population  
+            else:                                                       # regrowing population size
+                array = self.reproduceGrowing(j, n, modS,c,r)                #growing population
+
+            j = array[0] 
+            n = array[1]
+            p = j/n
+            simulation[0].append(currentGen)
+            simulation[1].append(p*n)
+            if p == 0:
+                simulation[2] = 0
+                break
+            if p == 1:
+                #print("BREAK, n ="+str(n))
+                simulation[2] = 1
+                break
+        return simulation
+    
+        ## runs several simulations and returns [[array of all simulation histories], float fixationProbability]
+        ## initialJ is the number of initial mutants, n is population size
+        ## bottleneckTimestamp is the time where a population bottleneck is simulated, reducing the population to decimation
+    def runSimulations(self,initialJ, n, mutationBenefit, generations = 1000, runs = 100, bottleneckTimestamp = 1100, decimation = 1, envMode = None,c=1000,r=1):
+        simulations = [[],0]
+        #print(runs)
+        for run in range(0, runs):
+            simulation = self.simulate(initialJ, n, mutationBenefit, generations, bottleneckTimestamp, decimation, envMode,c,r)     # bottleneck variables added
+            simulations[0].append(simulation)
+            simulations[1] = simulations[1] + simulation[2] 
+        if len(simulations[0]) > 0:  # Überprüfe, ob die Liste nicht leer ist
+            fixationProb = simulations[1] / len(simulations[0])
+            simulations[1] = fixationProb
+        else:
+            fixationProb = 0
+        simulations[1] = fixationProb
+        return simulations
+
+    ### takes current generation, unmodified selection coefficient and environment mode and returns modified selection coefficient
+    def environmentalBenefitModifier(self,gen, s, mode):
+        if mode is None:
+            return 1
+        if mode.lower() == "seasons":
+            return numpy.abs(self.sinusModifier(gen, s, 25, 2))
+        else:
+            return 1 
+    
+    ### takes current generation, unmodified selection coefficient and environment mode and returns modified selection coefficient modulated by periodic fluctuations of specified amplitude with a given period 
+    def sinusModifier(self, gen, s, period, amplitude):
+	    return s * numpy.sin((1/period)*gen) * amplitude
+    
+    def combinedSimulation(self):
+        population          = int(self.parameters[0])
+        maxGenerations      = int(self.parameters[1])
+        runs                = int(self.parameters[2])
+        c                   = int(self.parameters[3])
+        initialMutants      = int(self.parameters[4])
+        r                   = int(self.parameters[5])
+        fitnessBenefit      = self.parameters[6]
+        bottleneckTimestamp = int(self.parameters[7])
+        decimationFactor    = int(self.parameters[8])
+        plt.clf()
+        ### vanilla
+        #(initialJ, n, mutationBenefit, generations = 1000, runs = 100, bottleneckTimestamp = 1100, decimation = 1, envMode = None,c=1000,r=1):
+        noBottleneck = maxGenerations +1
+        simulations = self.runSimulations(initialMutants,population,fitnessBenefit, maxGenerations, runs,noBottleneck) #,1,None,c,r
+        vanillaFixation = str(simulations[1])
+        plt.plot(simulations[0][0][0],simulations[0][0][1],color="green", label = "standard, P(fix)="+vanillaFixation)
+        for simulation in simulations[0][::1]:
+            plt.plot(simulation[0],simulation[1],color="green")
+
+        ### seasonalVariation
+        simulations = self.runSimulations(initialMutants,population,fitnessBenefit, maxGenerations,runs,bottleneckTimestamp,decimationFactor,"Seasons",c,r)
+        seasonalVariationFixation = str(simulations[1])
+        plt.plot(simulations[0][0][0],simulations[0][0][1],color="blue", label = "seasonalVariation, P(fix)="+ seasonalVariationFixation)
+        for simulation in simulations[0][::1]:
+            plt.plot(simulation[0],simulation[1],color="blue")
+
+
+        ### bottleneck
+        simulations = self.runSimulations(initialMutants,population,fitnessBenefit, maxGenerations, runs,bottleneckTimestamp,decimationFactor,None,c,r)
+        bottleneckFixation = str(simulations[1])
+        plt.plot(simulations[0][0][0],simulations[0][0][1],color="red", label = "bottleneck, P(fix)="+bottleneckFixation)
+        for simulation in simulations[0][::1]:
+            plt.plot(simulation[0],simulation[1],color="red")
+
+        plt.xlabel("Generations")
+        plt.axvline(x = bottleneckTimestamp,color = 'r',linestyle='--')
+        plt.ylabel("p")
+
+
+        plt.suptitle("WRIGHT FISHER")
+        subtext = "initialMutants = "+str(initialMutants)+"; population = "+str(population)+"; fitnessBenefit ="+str(fitnessBenefit*100)+"%; maxGenerations = "+str(maxGenerations)+"; runs ="+str(runs)
+        plt.legend(title='Legend')
+        plt.title(subtext, fontsize=10, color='gray')
+        plt.show()
         
-## runs several simulations and returns [[array of all simulation histories], float fixationProbability]
-## initialJ is the number of initial mutants, n is population size
-## bottleneckTimestamp is the time where a population bottleneck is simulated, reducing the population to decimation
-def runSimulations(initialJ, n, mutationBenefit, generations = 1000, runs = 100, bottleneckTimestamp = 1100, decimation = 1, envMode = None):
-    simulations = [[],0]
-    for run in range(0, runs):
-        simulation = simulate(initialJ, n, mutationBenefit, generations, bottleneckTimestamp, decimation, envMode)     # bottleneck variables added
-        simulations[0].append(simulation)
-        simulations[1] = simulations[1] + simulation[2] 
-    fixationProb = simulations[1]/simulations[0].__len__()
-    simulations[1] = fixationProb
-    return simulations
-
-### takes current generation, unmodified selection coefficient and environment mode and returns modified selection coefficient
-def environmentalBenefitModifier(gen, s, mode):
-    if mode == None:
-        return 1
-    if mode.lower() == "seasons":
-        modS = numpy.abs(sinusModifier(gen, s, 25, 2))
-        return modS
-    else:
-        return 1 
-#else if mode == "random":
-
-
-
-### takes current generation, unmodified selection coefficient and environment mode and returns modified selection coefficient modulated by periodic fluctuations of specified amplitude with a given period 
-def sinusModifier(gen, s, period, amplitude):
-	modS = s * numpy.sin((1/period)*gen) * amplitude
-	return modS
-
-
-# SIMULATIONS
-initialMutants = 10
-population = 1000
-fitnessBenefit = 0.02
-maxGenerations = 1000
-runs = 200
-
-### vanilla
-simulations = runSimulations(initialMutants,population,fitnessBenefit, maxGenerations, runs,1100,1)
-vanillaFixation = str(simulations[1])
-plt.plot(simulations[0][0][0],simulations[0][0][1],color="green", label = "standard, P(fix)="+vanillaFixation)
-for simulation in simulations[0][::1]:
-    plt.plot(simulation[0],simulation[1],color="green")
-
-### seasonalVariation
-simulations = runSimulations(initialMutants,population,fitnessBenefit, maxGenerations,runs,1100,1,"Seasons")
-seasonalVariationFixation = str(simulations[1])
-plt.plot(simulations[0][0][0],simulations[0][0][1],color="blue", label = "seasonalVariation, P(fix)="+ seasonalVariationFixation)
-for simulation in simulations[0][::1]:
-    plt.plot(simulation[0],simulation[1],color="blue")
-
-
-### bottleneck
-bottleneckTimestamp = 100
-simulations = runSimulations(initialMutants,population,fitnessBenefit, maxGenerations, runs,bottleneckTimestamp,0.1)
-bottleneckFixation = str(simulations[1])
-plt.plot(simulations[0][0][0],simulations[0][0][1],color="red", label = "bottleneck, P(fix)="+bottleneckFixation)
-for simulation in simulations[0][::1]:
-    plt.plot(simulation[0],simulation[1],color="red")
-
-plt.xlabel("Generations")
-plt.axvline(x = bottleneckTimestamp,color = 'r',linestyle='--')
-plt.ylabel("p")
-
-
-plt.suptitle("WRIGHT FISHER")
-subtext = "initialMutants = "+str(initialMutants)+"; population = "+str(population)+"; fitnessBenefit ="+str(fitnessBenefit*100)+"%; maxGenerations = "+str(maxGenerations)+"; runs ="+str(runs)
-plt.legend(title='Legend')
-plt.title(subtext, fontsize=10, color='gray')
-plt.show()
-
+        return simulations
---
+++ GraphSim.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+from tkinter import ttk
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# imports EvoSim class
+from EvoSim import EvoSim
+
+class GraphSim:
+    def __init__(self, root):
+        self.root = root
+        self.parameters = [500, 1000, 10, 3000, 100, 0.9, 0.1, 0.1, 500, 0.1]  # Initializes parameter list
+        
+        # Erstellen Sie Schieberegler mit Echtzeit-Aktualisierung
+        slider_info = [
+            ("Population Size:", 1, 1000),
+            ("Generations:", 1, 1000),
+            ("Runs:", 1, 100),
+            ("Capacity:", 1, 2000),
+            ("Initial Mutants:", 0, 100),
+            ("Growth Rate:", 0, 1),
+            ("Mutation Benefit:", 0, 1),
+            ("Bottleneck Timestamp:", 1, 2000),
+            ("Decimation Factor:", 0, 1)
+        ]
+        self.slider_list = []
+
+        for label_text, min_value, max_value in slider_info:
+            label = ttk.Label(self.root, text=label_text)
+            label.pack()
+            slider = ttk.Scale(self.root, from_=min_value, to=max_value, orient="horizontal")
+            slider.set(self.parameters[self.slider_list.__len__()])
+            slider.pack()
+            slider.configure(command=lambda value, slider=slider: self.update_parameters(slider))
+            self.slider_list.append(slider)
+        
+        # self.create_graph()
+        self.simulation = EvoSim(self.parameters)  # creates EvoSim instance
+        self.update_parameters(slider)
+        
+        
+    # def create_graph(self):
+    #     self.figure, self.ax = plt.subplots(figsize=(8, 6))
+    #     self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
+    #     self.canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+    def update_parameters(self, slider):
+        self.parameters = [slider.get() for slider in self.slider_list]
+        self.simulation.parameters = self.parameters
+        simulation_results = self.run_simulation()
+        self.draw_graph(simulation_results)
+
+    def draw_graph(self,simulation_results):
+        self.ax.clear()
+        self.ax.set_xlabel("Generations")
+        self.ax.axvline(x=self.parameters[7], color='r', linestyle='--')
+        self.ax.set_ylabel("p")
+
+        for simulation in simulation_results[0]:
+            self.ax.plot(simulation[0], simulation[1], color="green")
+
+        plt.suptitle("WRIGHT FISHER")
+        subtext = f"initialMutants = {str(self.parameters[4])}; population = {str(self.parameters[0])}; fitnessBenefit ={str(self.parameters[6] * 100)}%; maxGenerations = {str(self.parameters[1])}; runs ={str(self.parameters[2])}"
+        plt.legend(title='Legend')
+        plt.title(subtext, fontsize=10, color='gray')
+        self.canvas.draw()  
+        
+    def run_simulation(self):
+        return self.simulation.combinedSimulation()
+
+def main():
+    root = tk.Tk()
+    root.title("Simulation of the Wright-Fisher-Model")
+    gui = GraphSim(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
+
+#     # Erstellen Sie Schieberegler mit Echtzeit-Aktualisierung
+    # def create_sliders(self):    
+    #     slider_info = [
+    #         ("Population Size:", 1, 1000),
+    #         ("Generations:", 1, 1000),
+    #         ("Runs:", 1, 100),
+    #         ("Capacity:", 1, 2000),
+    #         ("Initial Mutants:", 0, 100),
+    #         ("Growth Rate:", 0, 1),
+    #         ("Mutation Benefit:", 0, 1),
+    #         ("Bottleneck Timestamp:", 1, 2000),
+    #         ("Decimation Factor:", 0, 1)
+    #     ]
+    #     self.slider_list = []
+    #     print("Start Init")
+    #     for label_text, min_value, max_value in slider_info:
+    #         label = ttk.Label(self.root, text=label_text)
+    #         label.pack()
+    #         slider = ttk.Scale(self.root, from_=min_value, to=max_value, orient="horizontal", command=self.update_parameters)
+    #         slider.pack()
+    #         self.slider_list.append(slider)
+    #     print("Initialize")
+    #     self.booleanSlider = True
