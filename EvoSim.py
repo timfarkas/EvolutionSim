22fe16029b2579aa28114daeab34b9e25e109904
Johanna.Buth <Johannabuth@uni-konstanz.de>
2023-08-22 11:40:16 +0200

    Added right method for growing population

--- EvoSim.py
+++ EvoSim.py
@@ -1,33 +1,16 @@
 import numpy
 import matplotlib.pyplot as plt
 
-
-    # variables for bottleneck
-bottleneckTimestamp = 200
-decimationFactor = 0.1
-
 def reproduce(i, n, s):
     p = i/n
     expectedP = (p*(1+s))/(p*(1+s)+(1-p))
     j = numpy.random.binomial(n, expectedP)
     return j
-    
-# reproduction of growing population
-def reproduceGrowing(i, n, s, currentGen, bottleneckTimestamp):
-    c = 1000    # Capacity of the system
-    if currentGen < bottleneckTimestamp-1:                                   # calculation of new n
-        nNew = - c/(1 + numpy.exp(currentGen)) +n                               # before bottleneck
-    else:
-        nNew = - c/(1 + numpy.exp(currentGen - bottleneckTimestamp)) +n         # after bottleneck
-    
-    p = i/n
-    expectedP = (p*(1+s))/(p*(1+s)+(1-p))
-    j = numpy.random.binomial(n, expectedP)
-    return j
 
-def reproduceGrowing2(i, n, s):
+def reproduceGrowing(i, n, s):
     c = 1000    # Capacity of the system
-    nNew = int(n*(1-n/c))                       # before bottleneck
+    r = 0.1         # growth factor
+    nNew = int((1+r)*n*(1-(r*n)/((1+r)*c)))                       # before bottleneck
     
     p = i/n
     expectedP = (p*(1+s))/(p*(1+s)+(1-p))
@@ -44,11 +27,10 @@ def simulate(initialJ, n, mutationBenefit, generations = 1000, bottleneckTimesta
     for currentGen in range(0, generations):            
         ### modify mutationBenefit based on environmentalBenefitModifier to simulate environmental effects on mutationBenefit
         modS = mutationBenefit * environmentalBenefitModifier(currentGen,mutationBenefit,mode)
-        if currentGen < bottleneckTimestamp-1:                                 # situation before bottleneck
+        if currentGen < bottleneckTimestamp-1:      # situation before bottleneck
             
-            #newJ = reproduce(j, n, modS)                                             # constant population
-            #newJ = reproduceGrowing(j, n, modS, currentGen, bottleneckTimestamp)     #growing population
-            newJ = reproduceGrowing2(j, n, modS)   
+            #newJ = reproduce(j, n, modS)           # constant population  
+            newJ = reproduceGrowing(j, n, modS)     #growing population
             
             j = newJ  
         elif currentGen == bottleneckTimestamp-1:
@@ -56,15 +38,13 @@ def simulate(initialJ, n, mutationBenefit, generations = 1000, bottleneckTimesta
             n = int(n* decimationFactor)                            # decimates population size
             j = numpy.random.binomial(n, p)                                 # calculates new j
             p = j/n
-            #newJ = reproduce(j, n, modS)                                            # constant population
-            #newJ = reproduceGrowing(j, n, modS, currentGen, bottleneckTimestamp)     #growing population
-            newJ = reproduceGrowing2(j, n, modS)
+            #newJ = reproduce(j, n, modS)           # constant population  
+            newJ = reproduceGrowing(j, n, modS)     #growing population
             
             j = newJ
         else:                                                       # regrowing population size
-            #newJ = reproduce(j, n, modS)                                             # constant population
-            #newJ = reproduceGrowing(j, n, modS, currentGen, bottleneckTimestamp)     #growing population
-            newJ = reproduceGrowing2(j, n, modS)
+            #newJ = reproduce(j, n, modS)           # constant population  
+            newJ = reproduceGrowing(j, n, modS)     #growing population
             
             #print("Generation: "+str(currentGen)+" "+ str(j))
             j = newJ
@@ -110,18 +90,6 @@ def sinusModifier(gen, s, period, amplitude):
 	modS = s * numpy.sin((1/period)*gen) * amplitude
 	return modS
 
-
-### bottleneck
-simulations = runSimulations(50,1000,0.02, 1000, 100,500,0.1)
-for simulation in simulations[0][::3]:
-    plt.plot(simulation[0],simulation[1],color="red")
-plt.xlabel("Generations")
-plt.axvline(x=bottleneckTimestamp,color = 'r',linestyle='--')
-plt.ylabel("p")
-plt.title("Fixation probability: "+ str(simulations[1]))
-plt.show()
-bottleneckFixation = str(simulations[1])
-
 ### vanilla
 simulations = runSimulations(50,1000,0.02, 1000, 100,1100,1)
 for simulation in simulations[0][::3]:
@@ -138,6 +106,17 @@ plt.xlabel("Generations")
 plt.ylabel("p")
 seasonalVariationFixation = str(simulations[1])
 
+### bottleneck
+bottleneckTimestamp = 100
+simulations = runSimulations(50, 1000, 0.02, 1000, 100,bottleneckTimestamp,0.1)
+for simulation in simulations[0][::3]:
+    plt.plot(simulation[0],simulation[1],color="red")
+plt.xlabel("Generations")
+plt.axvline(x = bottleneckTimestamp,color = 'r',linestyle='--')
+plt.ylabel("p")
+plt.title("Fixation probability: "+ str(simulations[1]))
+
+bottleneckFixation = str(simulations[1])
 
 plt.title("P(fixation): \n"+"Vanilla: "+ vanillaFixation + "\nSeasons: "+ seasonalVariationFixation + "\nBottleneck: "+bottleneckFixation)
 plt.show()
